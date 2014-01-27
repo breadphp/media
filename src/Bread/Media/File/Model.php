e869c5d560d2d50db26e29312fb1d0822e8dacbe
tomasell <alex.tomasello@gmail.com>
2014-01-27 11:40:49 +0100

    Update model

--- src/Bread/Media/File/Model.php
+++ src/Bread/Media/File/Model.php
@@ -1,13 +1,51 @@
 <?php
-
 namespace Bread\Media\File;
 
-class Model
+use Bread\Configuration\Manager as Configuration;
+use Bread\REST;
+
+class Model extends REST\Model
 {
-//     use Bread\DAV\Properties;
+
+    public $id;
 
     public $name;
+
     public $type;
+
     public $data;
+
     public $size;
+
+    public function jsonSerialize()
+    {
+        return array(
+            'name' => $this->name,
+            'type' => $this->type,
+            'size' => $this->size,
+            'id' => $this->id
+        );
+    }
 }
+
+Configuration::defaults('Bread\Media\File\Model', array(
+    'properties' => array(
+        'id' => array(
+            'type'=>'integer',
+            'strategy' => 'autoincrement',
+            'unique' => true
+        ),
+        'name' => array(
+            'type' => 'string'
+        ),
+        'type' => array(
+            'type' => 'string'
+        ),
+        'data' => array(
+            'type' => 'resource'
+        ),
+        'size' => array(
+            'type' => 'integer'
+        )
+    )
+));
